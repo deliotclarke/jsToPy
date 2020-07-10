e991188064d468318b140d0d0192419e5d848f41
Daniel Clarke <deliotclarke@gmail.com>
2020-07-10 10:49:00 -0500

    added built in solve with len and set.

--- makeItPy.py
+++ makeItPy.py
@@ -47,3 +47,14 @@ def cleaner_unique_values(intList):
 
 cleaner_unique_values([1, 1, 1, 1, 1, 2])
 cleaner_unique_values([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])
+
+# stack overflow solutions
+
+
+def cleanest_unique_values(intList):
+    uniques = len(set(intList))
+    print("There are %d unique values in your list." % (uniques))
+
+
+cleanest_unique_values([1, 1, 1, 1, 1, 2])
+cleanest_unique_values([-2, -1, -1, 0, 1])
